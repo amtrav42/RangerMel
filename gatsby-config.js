f8f279248ae94c3fd3219dc664e0701e717ce02a
Amelia Travers <amelia.travers@myob.com>
2021-07-21 21:47:43 +1000

    sorted out config

--- gatsby-config.js
+++ gatsby-config.js
@@ -3,5 +3,22 @@ module.exports = {
     siteUrl: "https://www.yourdomain.tld",
     title: "RangerMel",
   },
-  plugins: [],
-};
+  plugins: [
+    {
+      resolve: `gatsby-source-filesystem`,
+      options: {
+        name: `THIS_IS_IT`,
+        path: `${__dirname}/src/markdown-pages/`,
+      },
+    },
+    // {
+    //   resolve: `gatsby-source-filesystem`,
+    //   options: {
+    //     name: `data`,
+    //     path: `${__dirname}/src/data/`,
+    //     ignore: [`**/\.*`], // ignore files starting with a dot
+    //   },
+    // },
+    `gatsby-plugin-mdx`,
+  ],
+}
